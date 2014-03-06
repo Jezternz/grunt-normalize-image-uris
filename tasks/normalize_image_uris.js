c676ae688558c6b9415d5db10907183d422ea52a
Jezternz <jezternz@gmail.com>
2014-03-06 14:45:52 +1030

    Added grunt plugin

---
+++ tasks/normalize_image_uris.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-normalize-image-uris
+ * https://github.com/Jezternz/grunt-normalize-image-uris
+ *
+ * Copyright (c) 2014 JoshM
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('normalize_image_uris', 'Normalize all images URI's located in HTML and CSS files to point to a single image directory, also move images into this directory.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
